05220e8aef6cfb727634d32231ddd944aaca2dfb
Alex Frenkel <sirshurf@yahoo.com>
2011-10-13 00:08:37 +0200

    added support for jqgrid multi column edit

--- controllers/RolesController.php
+++ controllers/RolesController.php
@@ -7,20 +7,23 @@ class User_RolesController extends Zend_Controller_Action
     public function indexAction ()
     {
 		$objUserTable = new User_Model_Db_Roles ();
-		$objSelect = $objUserTable->select ();
-		 
+		$objSelect = $objUserTable->select (TRUE);
+		
 		$objGrid = new Ingot_JQuery_JqGrid ( 'users', new Ingot_JQuery_JqGrid_Adapter_DbTableSelect ( $objSelect ) );
 		$objGrid->setIdCol ( User_Model_Db_Roles::COL_ID_ROLES ); 
-		$objGrid->setDblClkEdit(TRUE);
 		$objGrid->setLocalEdit();		
 				
 		$objGrid->addColumn ( new Ingot_JQuery_JqGrid_Column ( User_Model_Db_Roles::COL_ROLE, array('editable' => true, 'editrules'=> array('required'=> true)) ) );		
 		$objGrid->addColumn ( new Ingot_JQuery_JqGrid_Column ( User_Model_Db_Roles::COL_ROLE_TITLE, array('editable' => true, 'editrules'=> array('required'=> true)) ) );
 		
-		$objParentColumn =  new Ingot_JQuery_JqGrid_Column ( User_Model_Db_Roles::COL_ID_PARENT, array('editable' => true, 'editrules'=> array('required'=> true)) ); 
-		$objParentDecorator = new Ingot_JQuery_JqGrid_Column_Decorator_Search_Select($objParentColumn, array('value' => array(1=>1,2=>2)));
-		$objParentEditDecorator = new Ingot_JQuery_JqGrid_Column_Decorator_Edit_Select($objParentDecorator, array('value' => array(1=>1,2=>2)));
-		$objGrid->addColumn ($objParentDecorator );
+		
+		
+		Ingot_JQuery_JqGrid_Column_DoubleColumn::createSelectColumn($objGrid, 'Parent' );
+		
+		//$objParentColumn =  new Ingot_JQuery_JqGrid_Column ('parent_name', array('index' => User_Model_Db_Roles::COL_ID_PARENT) ); 
+		//$objParentDecorator = new Ingot_JQuery_JqGrid_Column_Decorator_Search_Select($objParentColumn, array('value' => array(1=>1,2=>2)));
+		//$objParentEditDecorator = new Ingot_JQuery_JqGrid_Column_Decorator_Edit_Select($objParentDecorator, array('value' => array(1=>1,2=>2)), array('required'=> true));
+		//$objGrid->addColumn ($objParentDecorator );
 		
 		$objGrid->addColumn ( new Ingot_JQuery_JqGrid_Column ( User_Model_Db_Roles::COL_ORDER, array('editable' => true,'edittype'=>'text' , 'editoptions' => array('defaultValue' => '99'), 'editrules'=> array('required'=> true, 'number'=>true)) ) );
 				
@@ -28,6 +31,7 @@ class User_RolesController extends Zend_Controller_Action
 		$objGridPager->setDefaultAdd ();
 		$objGridPager->setDefaultEdit ();
 		$objGridPager->setDefaultDel ();
+		$objGrid->setDblClkEdit(TRUE);
 		
 		$objGrid->registerPlugin ( new Ingot_JQuery_JqGrid_Plugin_ToolbarFilter () );
 		$this->view->grid = $objGrid->render ();
--- models/Db/Roles.php
+++ models/Db/Roles.php
@@ -1,22 +1,36 @@
 <?php
-class User_Model_Db_Roles extends SirShurf_Db_Table {
-    	
+
+class User_Model_Db_Roles extends SirShurf_Db_Table
+{
+
     /**
-	 * The default table name 
-	 */
-	const TBL_NAME = 'acl_roles';
-	
-	CONST COL_ID_ROLES = 'id_roles';
-	CONST COL_ROLE = "role";
-	CONST COL_ID_PARENT = "id_parent";
-	CONST COL_ROLE_TITLE = "title";
-	CONST COL_IS_ACTIVE = "is_active";
-	CONST COL_ORDER = "order";
-	
-	CONST COL_UPDATED_BY = 'updated_by';
-	CONST COL_UPDATED_ON = 'updated_on';
-	CONST COL_CREATED_BY = 'created_by';
-	CONST COL_CREATED_ON = 'created_on';
-	CONST COL_IS_DELETED = 'is_deleted';
-	
+     * The default table name 
+     */
+    const TBL_NAME = 'acl_roles';
+
+    CONST COL_ID_ROLES = 'id_roles';
+
+    CONST COL_ROLE = "role";
+
+    CONST COL_ID_PARENT = "id_parent";
+
+    CONST COL_ROLE_TITLE = "title";
+
+    CONST COL_IS_ACTIVE = "is_active";
+
+    CONST COL_ORDER = "order";
+
+    CONST COL_UPDATED_BY = 'updated_by';
+
+    CONST COL_UPDATED_ON = 'updated_on';
+
+    CONST COL_CREATED_BY = 'created_by';
+
+    CONST COL_CREATED_ON = 'created_on';
+
+    CONST COL_IS_DELETED = 'is_deleted';
+    
+    protected $_referenceMap = array(
+    'Parent' => array('columns' => array(self::COL_ID_PARENT), 'refTableClass' => 'User_Model_Db_Roles', 'refColumns' => array(self::COL_ID_ROLES), 'displayColumn' => self::COL_ROLE));
+
 }
